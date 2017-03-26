ef7d5c2d4dc96fcd00f93e029239bc482db293e7
Kusz Pawel <pawel.kusz@nokia.com>
2017-03-26 17:28:28 +0200

    Dodanie podsawowej funkcjonalnosci serwer
    Serwer wykonuje czynnosci inicjalizacyjne, po czym nasluchuje
    polaczen od klienta.

--- server.c
+++ server.c
@@ -1,12 +1,83 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <signal.h>
+#include <errno.h>
 
+
+#define MAXLINE 4096
+#define LISTENQ 10
 
 int main(int argc, char *argv[])
 {
-	if (argc != 2) {
-		fprintf(stdout, "Use example: %s PORT\n", argv[0]);
-		return 0;
-	}
+    int ret;
+    int listenfd, connfd;
+    char address[MAXLINE];
 
-	return 0;
+    socklen_t clilen;
+    pid_t childpid;
+
+    struct sockaddr_in cliaddr, servaddr;
+
+    if (argc != 2) {
+        fprintf(stdout, "Usage: %s <PORT>\n", argv[0]);
+        return -1;
+    }
+
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (listenfd < 0) {
+        fprintf(stderr, "Socket error");
+        return -1;
+    }
+
+    memset(&servaddr,'\0',sizeof(servaddr));
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port   = htons(atoi(argv[1]));
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    ret = bind(listenfd,(struct sockaddr *) &servaddr, sizeof(servaddr));
+    if (ret < 0) {
+        fprintf(stderr, "Bind error");
+        return -1;
+    }
+
+
+    ret = listen(listenfd,LISTENQ);
+    if (ret < 0) {
+        fprintf(stderr, "listen error");
+        return -1;
+    }
+
+    fprintf(stdout, "Server is running ... \n");
+
+
+    for ( ; ; ) {
+        clilen = sizeof(cliaddr);
+        if ((connfd = accept(listenfd,
+            (struct sockaddr *) &cliaddr, &clilen)) < 0 ) {
+            if (errno == EINTR)
+                continue;
+            else {
+                fprintf(stderr, "Accept error");
+                return 0;
+            }
+        }
+        fprintf(stdout, "Connection from %s, port %d \n",
+                inet_ntop(AF_INET,&cliaddr.sin_addr,address,sizeof(address)),
+                ntohs(cliaddr.sin_port));
+
+        if ( 0 == (childpid = fork())) {
+            close(listenfd);
+            //handler
+            exit(0);
+        }
+        close(connfd);
+    }
+
+    return 0;
 }
