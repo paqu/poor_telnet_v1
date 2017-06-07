b5a0b4756715384f85244ffc4ea06f1869b4af2d
Kusz Pawel <pawel.kusz@nokia.com>
2017-06-07 16:52:56 +0200

    Zamiana tabow na spacje

--- client.c
+++ client.c
@@ -10,35 +10,35 @@
 
 int main(int argc, char *argv[])
 {
-	int ret;
-	int sockfd;
-	struct sockaddr_in servaddr;
+    int ret;
+    int sockfd;
+    struct sockaddr_in servaddr;
 
-	if (argc != 3) {
-		fprintf(stdout, "Usage %s <SERV_ADDR> <PORT>\n", argv[0]);
-		return -1;
-	}
+    if (argc != 3) {
+        fprintf(stdout, "Usage %s <SERV_ADDR> <PORT>\n", argv[0]);
+        return -1;
+    }
 
-	sockfd = socket(AF_INET, SOCK_STREAM,0);
-	if (sockfd < 0) {
-		fprintf(stderr, "Socker error");
-		return -1;
-	}
+    sockfd = socket(AF_INET, SOCK_STREAM,0);
+    if (sockfd < 0) {
+        fprintf(stderr, "Socker error");
+        return -1;
+    }
 
 
-	memset(&servaddr,'\0',sizeof(servaddr));
+    memset(&servaddr,'\0',sizeof(servaddr));
 
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_port   = htons(atoi(argv[2]));
-	inet_pton(AF_INET,argv[1], &servaddr.sin_addr);
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port   = htons(atoi(argv[2]));
+    inet_pton(AF_INET,argv[1], &servaddr.sin_addr);
 
-	ret = connect(sockfd, (struct sockaddr *) &servaddr,sizeof(servaddr));
-	if (ret < 0) {
-		fprintf(stderr, "Connet error");
-		return -1;
-	}
+    ret = connect(sockfd, (struct sockaddr *) &servaddr,sizeof(servaddr));
+    if (ret < 0) {
+        fprintf(stderr, "Connet error");
+        return -1;
+    }
 
-	// Client action
-	close(sockfd);
-	exit(0);
+    // Client action
+    close(sockfd);
+    exit(0);
 }
